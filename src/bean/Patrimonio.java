b827edbb074e24420a666be788775af82ad92034
MatheusHNSantos <matheushenriki10@hotmail.com>
2018-03-04 20:47:26 -0300

    refatoração de nome de pacote

--- src/bean/Patrimonio.java
+++ src/bean/Patrimonio.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package model.bean;
+package bean;
 
 import java.util.ArrayList;
-import model.dao.PatrimonioDAO;
+import dao.PatrimonioDAO;
 
 /**
  *
--- src/dao/UsuarioDAO.java
+++ src/dao/UsuarioDAO.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package model.dao;
+package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -11,7 +11,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
-import model.bean.Usuario;
+import bean.Usuario;
 
 /**
  *
