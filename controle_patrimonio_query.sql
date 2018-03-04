f7ab984069d14a7b750b1a4c44b6c9c9f34d88b5
MatheusHNSantos <matheushenriki10@hotmail.com>
2018-03-04 18:01:10 -0300

    criação query

---
+++ controle_patrimonio_query.sql
@@ -0,0 +1,29 @@
+create database if not exists controle_patrimonio;
+
+use controle_patrimonio;
+
+create table if not exists usuario(
+	login_usuario varchar(20) not null,
+    senha_usuario varchar(20) not null,
+    conf_senha_usuario varchar(20),
+    nome_usuario varchar(30) not null,
+    funcao_usuario varchar(20),
+	cell_usuario varchar(20),
+    nivel_Acesso int not null,
+    constraint prk_login_usuario primary key (login_usuario)
+);
+
+create table if not exists patrimonio(
+	cod_patrimonio int not null,
+	num_proc_entrada int not null,
+	num_nota_fiscal int not null,
+	nome_patrimonio varchar(30) not null,
+	marca_patrimonio varchar(30),
+	modelo_patrimonio varchar(30),
+	num_serie_patrimonio int not null,
+	valor_patrimonio float not null,
+	mod_aqu_patrimonio varchar(30),
+	local_patrimonio varchar(30),
+	baixa_patrimonio bit,
+    constraint prk_cod_patrimonio primary key (cod_patrimonio)
+);
--- src/model/dao/ConnectionFactory.java
+++ src/model/dao/ConnectionFactory.java
@@ -21,7 +21,7 @@ import java.util.logging.Logger;
 public class ConnectionFactory {
 
     private static final String DRIVER = "com.mysql.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/teste";
+    private static final String URL = "jdbc:mysql://localhost:3306/controle_patrimonio";
     private static final String USER = "root";
     private static final String PASS = "";
 
