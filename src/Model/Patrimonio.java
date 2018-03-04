ddf8bc886e8c69bf17cfcb39ae98b1264c0f73b8
dlpires <dlpires@outlook.com>
2018-03-04 17:27:15 -0300

    ControlePatrimonio - Classes V0.1

---
+++ src/Model/Patrimonio.java
@@ -0,0 +1,222 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author dlpires
+ */
+public class Patrimonio {
+
+    //ATRIBUTOS
+    
+    private int CodPatrimonio;
+    private int NumProcEntrada;
+    private int NumNotaFiscal;
+    private String NomePatrimonio;
+    private String MarcaPatrimonio;
+    private String ModeloPatrimonio;
+    private int NumSeriePatrimonio;
+    private String DescricaoPatrimonio;
+    private double ValorPatrimonio;
+    private String ModAquPatrimonio;
+    private String LocalPatrimonio;
+    private boolean BaixaPatrimonio;
+    
+    /**
+     * @return the CodPatrimonio
+     */
+    public int getCodPatrimonio() {
+        return CodPatrimonio;
+    }
+
+    /**
+     * @param CodPatrimonio the CodPatrimonio to set
+     */
+    public void setCodPatrimonio(int CodPatrimonio) {
+        this.CodPatrimonio = CodPatrimonio;
+    }
+
+    /**
+     * @return the NumProcEntrada
+     */
+    public int getNumProcEntrada() {
+        return NumProcEntrada;
+    }
+
+    /**
+     * @param NumProcEntrada the NumProcEntrada to set
+     */
+    public void setNumProcEntrada(int NumProcEntrada) {
+        this.NumProcEntrada = NumProcEntrada;
+    }
+
+    /**
+     * @return the NumNotaFiscal
+     */
+    public int getNumNotaFiscal() {
+        return NumNotaFiscal;
+    }
+
+    /**
+     * @param NumNotaFiscal the NumNotaFiscal to set
+     */
+    public void setNumNotaFiscal(int NumNotaFiscal) {
+        this.NumNotaFiscal = NumNotaFiscal;
+    }
+
+    /**
+     * @return the NomePatrimonio
+     */
+    public String getNomePatrimonio() {
+        return NomePatrimonio;
+    }
+
+    /**
+     * @param NomePatrimonio the NomePatrimonio to set
+     */
+    public void setNomePatrimonio(String NomePatrimonio) {
+        this.NomePatrimonio = NomePatrimonio;
+    }
+
+    /**
+     * @return the MarcaPatrimonio
+     */
+    public String getMarcaPatrimonio() {
+        return MarcaPatrimonio;
+    }
+
+    /**
+     * @param MarcaPatrimonio the MarcaPatrimonio to set
+     */
+    public void setMarcaPatrimonio(String MarcaPatrimonio) {
+        this.MarcaPatrimonio = MarcaPatrimonio;
+    }
+
+    /**
+     * @return the ModeloPatrimonio
+     */
+    public String getModeloPatrimonio() {
+        return ModeloPatrimonio;
+    }
+
+    /**
+     * @param ModeloPatrimonio the ModeloPatrimonio to set
+     */
+    public void setModeloPatrimonio(String ModeloPatrimonio) {
+        this.ModeloPatrimonio = ModeloPatrimonio;
+    }
+
+    /**
+     * @return the NumSeriePatrimonio
+     */
+    public int getNumSeriePatrimonio() {
+        return NumSeriePatrimonio;
+    }
+
+    /**
+     * @param NumSeriePatrimonio the NumSeriePatrimonio to set
+     */
+    public void setNumSeriePatrimonio(int NumSeriePatrimonio) {
+        this.NumSeriePatrimonio = NumSeriePatrimonio;
+    }
+
+    /**
+     * @return the DescricaoPatrimonio
+     */
+    public String getDescricaoPatrimonio() {
+        return DescricaoPatrimonio;
+    }
+
+    /**
+     * @param DescricaoPatrimonio the DescricaoPatrimonio to set
+     */
+    public void setDescricaoPatrimonio(String DescricaoPatrimonio) {
+        this.DescricaoPatrimonio = DescricaoPatrimonio;
+    }
+
+    /**
+     * @return the ValorPatrimonio
+     */
+    public double getValorPatrimonio() {
+        return ValorPatrimonio;
+    }
+
+    /**
+     * @param ValorPatrimonio the ValorPatrimonio to set
+     */
+    public void setValorPatrimonio(double ValorPatrimonio) {
+        this.ValorPatrimonio = ValorPatrimonio;
+    }
+
+    /**
+     * @return the ModAquPatrimonio
+     */
+    public String getModAquPatrimonio() {
+        return ModAquPatrimonio;
+    }
+
+    /**
+     * @param ModAquPatrimonio the ModAquPatrimonio to set
+     */
+    public void setModAquPatrimonio(String ModAquPatrimonio) {
+        this.ModAquPatrimonio = ModAquPatrimonio;
+    }
+
+    /**
+     * @return the LocalPatrimonio
+     */
+    public String getLocalPatrimonio() {
+        return LocalPatrimonio;
+    }
+
+    /**
+     * @param LocalPatrimonio the LocalPatrimonio to set
+     */
+    public void setLocalPatrimonio(String LocalPatrimonio) {
+        this.LocalPatrimonio = LocalPatrimonio;
+    }
+
+    /**
+     * @return the BaixaPatrimonio
+     */
+    public boolean isBaixaPatrimonio() {
+        return BaixaPatrimonio;
+    }
+
+    /**
+     * @param BaixaPatrimonio the BaixaPatrimonio to set
+     */
+    public void setBaixaPatrimonio(boolean BaixaPatrimonio) {
+        this.BaixaPatrimonio = BaixaPatrimonio;
+    }
+    
+    //MÉTODOS 
+    
+    public void Cadastrar(){
+        
+    }
+    
+    public void Consultar(){
+        
+    }
+    
+    public void Alterar(){
+        
+    }
+    
+    public void Excluir(){
+        
+    }
+    
+    public void GerarRelatorio(){
+        
+    }
+    
+    public void GerarRelatorioBaixa(){
+        
+    }
+}
---
+++ src/Model/Usuario.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author dlpires
+ */
+public class Usuario {
+
+    private String LoginUsuario;
+    private String SenhaUsuario;
+    private String ConfSenhaUsuario;
+    private String NomeUsuario;
+    private String FuncaoUsuario;
+    private String CellUsuario;
+    
+    /**
+     * @return the LoginUsuario
+     */
+    public String getLoginUsuario() {
+        return LoginUsuario;
+    }
+
+    /**
+     * @param LoginUsuario the LoginUsuario to set
+     */
+    public void setLoginUsuario(String LoginUsuario) {
+        this.LoginUsuario = LoginUsuario;
+    }
+
+    /**
+     * @return the SenhaUsuario
+     */
+    public String getSenhaUsuario() {
+        return SenhaUsuario;
+    }
+
+    /**
+     * @param SenhaUsuario the SenhaUsuario to set
+     */
+    public void setSenhaUsuario(String SenhaUsuario) {
+        this.SenhaUsuario = SenhaUsuario;
+    }
+
+    /**
+     * @return the ConfSenhaUsuario
+     */
+    public String getConfSenhaUsuario() {
+        return ConfSenhaUsuario;
+    }
+
+    /**
+     * @param ConfSenhaUsuario the ConfSenhaUsuario to set
+     */
+    public void setConfSenhaUsuario(String ConfSenhaUsuario) {
+        this.ConfSenhaUsuario = ConfSenhaUsuario;
+    }
+
+    /**
+     * @return the NomeUsuario
+     */
+    public String getNomeUsuario() {
+        return NomeUsuario;
+    }
+
+    /**
+     * @param NomeUsuario the NomeUsuario to set
+     */
+    public void setNomeUsuario(String NomeUsuario) {
+        this.NomeUsuario = NomeUsuario;
+    }
+
+    /**
+     * @return the FuncaoUsuario
+     */
+    public String getFuncaoUsuario() {
+        return FuncaoUsuario;
+    }
+
+    /**
+     * @param FuncaoUsuario the FuncaoUsuario to set
+     */
+    public void setFuncaoUsuario(String FuncaoUsuario) {
+        this.FuncaoUsuario = FuncaoUsuario;
+    }
+
+    /**
+     * @return the CellUsuario
+     */
+    public String getCellUsuario() {
+        return CellUsuario;
+    }
+
+    /**
+     * @param CellUsuario the CellUsuario to set
+     */
+    public void setCellUsuario(String CellUsuario) {
+        this.CellUsuario = CellUsuario;
+    }
+}
